8c5c0c30752bf722d629d5398f19f3e771667f3f
AlanBenjaDev <alenciopalencio@gmail.com>
2025-08-03 16:21:44 -0300

    solucion porfavor

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import db from './db.js';
+import db from './db.js'; // conexión importada correctamente
 import usuarios from './usuarios.js';
 import productos from './productos.js';
 import adminData from './adminData.js';
@@ -8,22 +8,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
-const corsOptions = {
-  origin: process.env.URLFRONTEND, 
-  credentials: true,
-  optionsSuccessStatus: 200 
-};
+app.use(cors({
+origin:process.env.URLFRONTEND,
+credentials:true
 
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
-
+}));
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads')); 
 
-app.use('/api/admin', adminData);
+app.use('/api/admin', adminData)
 app.use('/api/usuarios', usuarios);
 app.use('/api/productos', productos);
 
