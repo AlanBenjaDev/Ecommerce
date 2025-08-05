39c59c82944d83c81727c8b277ddba91dca16d22
AlanBenjaDev <alenciopalencio@gmail.com>
2025-08-04 23:40:24 -0300

    solucion del proyecto

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -1,43 +1,24 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import dotenv from 'dotenv'
-dotenv.config()
-(async function() {
+import dotenv from 'dotenv';
+dotenv.config();
 
-
+// ✅ Configuración de Cloudinary
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
-    
-    
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-  
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
 
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'ecommerce', // Podés cambiar el nombre de carpeta
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  }
+});
+
+
+export { storage };
